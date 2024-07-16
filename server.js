9202805ab8fd67990602bf863bb5bdbd9af78c1b
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-07-16 06:06:09 +0000

    requests

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('Listening for requests on port 3000')
+});
+
+// localhost is like a domain name that takes us to a loopback ip address that points back to our own computer
