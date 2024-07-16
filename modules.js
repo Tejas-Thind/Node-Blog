57c2a5a9d3affa24203934b4c4e0751ec318e1e4
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-07-16 05:10:35 +0000

    modules

---
+++ modules.js
@@ -0,0 +1,7 @@
+const { people, ages } = require('./people'); // runs the peoples file, xyz is an empty object
+
+console.log(people, ages);
+
+const os = require('os');
+
+console.log(os.platform(), os.homedir());
---
+++ people.js
@@ -0,0 +1,9 @@
+const people = ['Guy', 'guy2', 'guy3', 'guy4']
+const ages = [21, 22, 23, 24]
+
+console.log(people);
+
+module.exports = {
+    people, // shortcut for people: people
+    ages
+};
