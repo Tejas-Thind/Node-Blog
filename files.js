23ca82bb65e9a1f3f46138fd8e0135eab1cef3cf
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-07-16 05:24:48 +0000

    done files

--- files.js
+++ files.js
@@ -34,5 +34,10 @@ if (!fs.existsSync('./assets')) {
 
 // Deleting files
 if (fs.existsSync('./docs/deleteme.txt')) {
-
+    fs.unlink('./docs/deleteme.txt', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('file deleted');
+    });
 }
