eedd74f09926e1ed7f0dbf9b94779338619f4f44
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-07-17 06:22:25 +0000

    routing

--- app.js
+++ app.js
@@ -7,5 +7,9 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send();
+    res.sendFile('./views/index.html', { root: __dirname  });
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname  });
 });
