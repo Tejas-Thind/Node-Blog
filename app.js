51d57ff43bcd118f92c3e6a1293c16bef5cb7a95
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-07-17 23:48:45 +0000

    ejs pages

--- app.js
+++ app.js
@@ -14,16 +14,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname  });
+    res.render('about');
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
 });
-
 
 // 404 page
 app.use((req, res) => { // fire for every single request coming in but only if it reaches this point in the code
-    res.status(404).sendFile('./views/404.html', { root: __dirname  });
+    res.status(404).render('404');
 });
