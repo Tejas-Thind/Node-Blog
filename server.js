b63278d4dae0375798a41a633dcc95bc24d99312
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-07-17 05:21:48 +0000

    routing

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     // set header content type
     res.setHeader('Content-Type', 'text/html'); // what type of content we are sending
+    
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html'
+    }
 
-    res.write('<p>Hello ninjas</p>');
-    res.end();
+    // send an html file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end()
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
 });
 
 server.listen(3000, 'localhost', () => {
