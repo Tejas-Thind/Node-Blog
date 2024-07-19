c2343c419a148d6d1267c20ec706f268c8ecfb40
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-07-19 06:18:02 +0000

    used prettier

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-const express = require('express');
+const express = require("express");
 
 // express app
 const app = express();
 
 // ergister view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // listen for requests
 app.listen(3000);
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/about', (req, res) => {
-    res.render('about');
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create');
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 // 404 page
-app.use((req, res) => { // fire for every single request coming in but only if it reaches this point in the code
-    res.status(404).render('404');
+app.use((req, res) => {
+  // fire for every single request coming in but only if it reaches this point in the code
+  res.status(404).render("404");
 });
