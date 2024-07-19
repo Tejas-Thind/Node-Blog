e670ee2082ad0fa88ee79dfb74328bbf40201175
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-07-19 20:11:34 +0000

    middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const express = require("express");
+const morgan = require('morgan');
 
 // express app
 const app = express();
@@ -7,27 +8,37 @@ const app = express();
 app.listen(3000);
 
 // register view engine
-app.set('view engine', 'ejs');
-// app.set('views', 'myviews');
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
+app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
   const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
   ];
-  res.render('index', { title: 'Home', blogs: blogs });
+  res.render("index", { title: "Home", blogs: blogs });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
 });
 
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404', { title: '404' });
+  res.status(404).render("404", { title: "404" });
 });
