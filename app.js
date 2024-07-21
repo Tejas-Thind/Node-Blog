b7a4e7ebeca1d30bf63b4a5774034d6207bb9650
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-07-21 20:39:50 +0000

    more mongoose

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const express = require("express");
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
-const dbURI = 'mongodb+srv://tejasst0544:Mongo2024@cluster0.jjf6s6z.mongodb.net/ninja-blog?retryWrites=true&w=majority&appName=Cluster0'
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => console.log('Connected to DB'))
-  .catch(err => console.log(err));
+const dbURI =
+  "mongodb+srv://tejasst0544:Mongo2024@cluster0.jjf6s6z.mongodb.net/ninja-blog?retryWrites=true&w=majority&appName=Cluster0";
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    console.log("Connected to DB");
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
 
 // register view engine
 app.set("view engine", "ejs");
 
-// listen for requests
-app.listen(3000);
-
 // middleware and static files
-app.use(express.static('public')); // pass a folder for any files inside to be made accessible to the front end
-app.use(morgan('dev')); // using middleware to log info to the console about the request made
+app.use(express.static("public")); // pass a folder for any files inside to be made accessible to the front end
+app.use(morgan("dev")); // using middleware to log info to the console about the request made
+
+// mongoose sandbox
+app.get('/add-blog',(req, res) => {
+  const blog = new Blog(); // New instance of blog document and pass a object with properties
+});
 
 app.get("/", (req, res) => {
   const blogs = [
