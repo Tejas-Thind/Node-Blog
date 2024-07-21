5e9ed8f8c2a76adc218cd3fc5460d6f8eb1c2f9f
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-07-21 04:16:19 +0000

    mongoose

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 // express app
 const app = express();
 
-const dbURI = 'mongodb+srv://tejasst0544:Mongo44@cluster0.jjf6s6z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const dbURI = 'mongodb+srv://tejasst0544:Mongo2024@cluster0.jjf6s6z.mongodb.net/ninja-blog?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => console.log('Connected to DB'))
+  .catch(err => console.log(err));
 
 // register view engine
 app.set("view engine", "ejs");
@@ -13,7 +17,7 @@ app.set("view engine", "ejs");
 app.listen(3000);
 
 // middleware and static files
-app.use(express.static('public')) // pass a folder for any files inside to be made accesible to the front end
+app.use(express.static('public')); // pass a folder for any files inside to be made accessible to the front end
 app.use(morgan('dev')); // using middleware to log info to the console about the request made
 
 app.get("/", (req, res) => {
