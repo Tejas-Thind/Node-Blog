756466673ce745b58bae866e46340323c0d5705d
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-07-23 18:17:48 +0000

    delete req

--- app.js
+++ app.js
@@ -45,25 +45,34 @@ app.get("/blogs", (req, res) => {
     });
 });
 
-app.post('/blogs', (req, res) => {
+app.post("/blogs", (req, res) => {
   const blog = new Blog(req.body);
-  blog.save() // async
-  .then((result) => {
-    res.redirect('/blogs')
-  })
-  .catch(err => {
-    console.log(err);
-  })
+  blog
+    .save() // async
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-app.get('/blogs/:id', (req, res) => {
+app.get("/blogs/:id", (req, res) => {
   const id = req.params.id;
   Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: "Blog Details" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.delete("/blogs/:id", (req, res) => {
+  const id = request.params.id;
+  Blog.findByIdAndDelete(id)
   .then(result => {
-    res.render('details', {blog: result, title: 'Blog Details'})
-  })
-  .catch(err => {
-    console.log(err);
+    
   })
 });
 
